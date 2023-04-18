3a4bf6b7572b53b8f16ef333ff4fcb227741138f
Narkobaron15 <oleksii120@gmail.com>
2023-04-18 22:54:06 +0300

    initial

---
+++ src/app/mock.ts
@@ -0,0 +1,16 @@
+import { ITask, Task } from "./task";
+
+export const TASK_MOCK: ITask[] = [
+    new Task('Hit the gym'),
+    new Task('Pay bills', true, true),
+    new Task('Meet John'),
+    new Task('Buy eggs', false, true, new Date(2023, 2, 29)),
+    new Task('Read a book'),
+    new Task('Organize office'),
+    new Task('Eat dinner'),
+    new Task('Buy apples', false, true, new Date(2023, 2, 5)),
+    new Task('Meet George', false, true),
+    new Task('Feed the cat'),
+    new Task('Write a letter', true),
+    new Task('Run 1 km', false, false, new Date(2022, 0, 15)),
+];
